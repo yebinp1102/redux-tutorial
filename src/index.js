6a7fafb64628359c2134fadd3edb86479677c341
yebin Park <yebinpark@yebinui-iMac.local>
2022-02-13 02:13:57 +0900

    make to do list without redux(only js)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = ( count = 0, action) => {
-  switch(action.type){
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createToDo = toDo => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 }
 
-const countStore = createStore(countModifier);
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 }
-countStore.subscribe(onChange);
 
-const handleAdd = () => {
-  countStore.dispatch({type: ADD})
-}
-const handleMius = () => {
-  countStore.dispatch({type: MINUS})
-}
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMius);
+form.addEventListener("submit", onSubmit);
